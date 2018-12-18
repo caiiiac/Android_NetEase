61c13b1dbee4d9e447d4b413b7f562b66b03398a
caijq <caiiiac@163.com>
2018-12-18 14:30:10 +0800

    add DetailActivity

---
+++ app/src/main/java/com/simple/neteasesimple/news/activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.simple.neteasesimple.news.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.simple.neteasesimple.R;
+import com.simple.neteasesimple.until.BaseActivity;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
